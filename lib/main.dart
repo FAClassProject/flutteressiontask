1ee88e3b5fe01e7e6f8e39940e8bdf4150dbc6b6
KiCodes <harold.ekpo@yahoo.com>
2023-04-07 00:12:17 +0100

    new logo, class texts.dart,

--- lib/main.dart
+++ lib/main.dart
@@ -33,68 +33,89 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepPurpleAccent,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/images/flutter_logo.png',
-            scale: 2.3,
-          ),
-          Text(
-            Textlines().first,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white70,
-              fontSize: 42,
-              wordSpacing: -10,
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-            Textlines().second,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 22,
-            ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.white),
-              shape: MaterialStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        color: Colors.deepPurpleAccent,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 843 / 6,
+              width: 1043 / 6,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                  ),
+              child: Image.asset(
+                'assets/images/logo_flutter_1080px_clr.png',
+                scale: 15.3,
               ),
-              minimumSize: MaterialStateProperty.all(Size(240, 40)),
             ),
-            onPressed: () {},
-            child: Text(
-              Textlines().third,
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              Textlines().first,
+              textAlign: TextAlign.center,
               style: TextStyle(
-                color: Colors.deepPurpleAccent,
-                fontSize: 15,
+                color: Colors.white70,
+                fontSize: 42,
+                wordSpacing: -10,
               ),
             ),
-          ),
-          SizedBox(height: 110),
-          Text(
-            Textlines().fourth,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 22,
+            SizedBox(
+              height: 30,
             ),
-          ),
-        ],
+            Text(
+              Textlines().second,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 22, fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 86,),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                    minimumSize: MaterialStateProperty.all(Size(310, 55)),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    Textlines().third,
+                    style: TextStyle(
+                      color: Colors.deepPurpleAccent,
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Text(
+              Textlines().fourth,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 19,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/texts.dart
+++ lib/texts.dart
@@ -1,5 +1,5 @@
 class Textlines{
-  final first = 'Hi there,\nI\'m Reflectly';
+  final first = 'Hi  there, \nI\'m  Reflectly';
   final second = 'Your new Personal \nJournaling companion';
   final third = 'Hi, Reflectly';
   final fourth = 'I ALREADY HAVE AN ACCOUNT';
